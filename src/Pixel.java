890a16c3a84e5167080e664220d7424af9d12491
AnthonyCarmichael <anthonycarmichael0@gmail.com>
2024-02-14 19:42:58 -0500

    Premier commit avant JavaDoc

--- src/Pixel.java
+++ src/Pixel.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Pixel {
+public class Pixel {
+    protected int _couleur;
+
+    public Pixel()
+    {
+        _couleur = 0;
+    }
+    public Pixel(int couleur)
+    {
+        _couleur = couleur;
+    }
+
+    public int get_couleur() {
+        return _couleur;
+    }
+    public void set_couleur(int _couleur) {
+        this._couleur = _couleur;
+    }
+    public void draw() {
+    }
+
+
 }

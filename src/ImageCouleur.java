781699db5c010fff23e1a92548f245d410729f9e
Richard-Dongmo <dongmorichard01@gmail.com>
2024-02-19 16:33:06 -0500

    squelette imageNoirBlanc et couleur

---
+++ src/ImageCouleur.java
@@ -0,0 +1,19 @@
+public class ImageCouleur extends Image{
+
+    //Retourne la couleur prépondérante
+    public Pixel couleur_preponderante(){
+        Pixel preponderant = new Pixel();
+
+        return preponderant;
+    }
+
+    //Modifie la valeur du pixel
+    public void eclaircir_noicir(int valeur){
+
+    }
+
+    //Réduit le pixel
+    public void reduire(){
+
+    }
+}
---
+++ src/ImageNoirBlanc.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author Anthony Carmichael, Maxime Malette, Richard Dongmo
+ * @version 1.0
+ *
+ * cette classe implemente une image en noir et blanc
+ */
+
+public class ImageNoirBlanc extends Image{
+
+    //Retourne la couleur prépondérante
+    public Pixel couleur_preponderante(){
+        Pixel preponderant = new Pixel();
+
+        return preponderant;
+    }
+
+    //Modifie la valeur du pixel
+    public void eclaircir_noicir(int valeur){
+
+    }
+
+    //Réduit le pixel
+    public void reduire(){
+
+    }
+}
